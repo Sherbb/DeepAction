cd448d80bcf6dd3733d14cd2aee0c76a382bc19e
Sherbb <alansherba@gmail.com>
2022-01-09 18:34:24 -0500

    removed all the junk

--- DeepAction/Assets/DeepAction/Core/Enums/D_State.cs
+++
@@ -1,8 +0,0 @@
-//states should be boolean type...things
-//for example a SLOW would usually not be a state because the slow % could varry.
-
-public enum D_State
-{
-    stunned,
-    silenced,    
-}
