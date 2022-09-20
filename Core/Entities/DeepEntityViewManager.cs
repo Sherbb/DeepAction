ddebffdaaca3f25ddfa7cb210f82aa3e895f640a
Sherbb <alansherba@gmail.com>
2022-09-20 16:23:01 -0400

    wip (sync)

---
+++ Core/Entities/DeepEntityViewManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DeepAction
+{
+    public class DeepEntityViewManager : MonoBehaviour
+    {
+        public DeepEntity parent { get; private set; }
+
+        public void InitializeViewManager()
+        {
+
+        }
+
+
+        public void RegisterView()
+        {
+
+        }
+
+        public void RemoveView()
+        {
+
+        }
+    }
+}
